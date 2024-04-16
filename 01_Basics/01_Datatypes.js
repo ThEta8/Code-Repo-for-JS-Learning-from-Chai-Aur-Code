942aedf6a794e2769fd74a2a6b74732426b667a5
ThEta8 <72531767+ThEta8@users.noreply.github.com>
2024-04-16 11:29:31 +0000

    Datatype file Add

---
+++ 01_Basics/01_Datatypes.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const id = 536438
+let AccountName = "@Google.com"
+var accountPass = "123454567"
+let isAccountLoggedIn = true
+// null : Empty Value
+//undefined : Variable not defined
+// Symbol : Unique
