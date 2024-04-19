e0c19ee3aa9b573bae3f5c5b4582eca2a0eb8fc8
ThEta8 <72531767+ThEta8@users.noreply.github.com>
2024-04-19 09:15:56 +0000

    aDDING ARRAY

--- 02_Basics/01_Array.js
+++ 02_Basics/01_Array.js
@@ -1,4 +1,4 @@
-const aArray = [0,1,2,3,4,4]
+const aArray = [0,1,2,3,4,5]
 
 //Adds Element in End of Array
 //aArray.push(6)
@@ -6,11 +6,11 @@ const aArray = [0,1,2,3,4,4]
 //unshift Adds Element in start of Array
 //aArray.unshift(-1)
 // aArray.shift(-1)
-// console.log(aArray);
+// console.log(aArray)
 
 ///onsole.log(aArray.lastIndexOf(4))
 
 
 // Slice, Splice
-let = 
+let = ""
 
